bd08c52894970cfe5a52a24f7cbcfac1b33aba0a
SHRIBHARANIPRIYA_N <123936496+Shribharanipriya-N@users.noreply.github.com>
2023-09-01 10:09:24 +0530

    Add files via upload

---
+++ Know-My-Nation-main/scripts/script-page.js
@@ -0,0 +1,127 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const queryParams = new URLSearchParams(window.location.search);
+
+  var togglebtn = document.getElementById("toggle");
+  var sidebarbtn = document.getElementById("sidebar");
+
+  togglebtn.onclick = function () {
+    togglebtn.classList.toggle("active");
+    sidebarbtn.classList.toggle("active");
+  };
+  //Receiving Datas...
+
+  const state_name = queryParams.get("state_name");
+  const state_imgs = queryParams.get("state_imgs");
+  const tourist_imgs = queryParams.get("tourist_imgs");
+  const tourist_names = queryParams.get("tourist_names");
+
+  const state_maplink = queryParams.get("state_maplink");
+  const state_capital = queryParams.get("state_capital");
+  const state_cm = queryParams.get("state_cm");
+  const state_governor = queryParams.get("state_governor");
+  const state_districts = queryParams.get("state_districts");
+  const state_populations = queryParams.get("state_populations");
+  const state_area = queryParams.get("state_area");
+  const state_literacy = queryParams.get("state_literacy");
+  const state_gdp = queryParams.get("state_gdp");
+  const state_symbol = queryParams.get("state_symbol");
+  const state_animal = queryParams.get("state_animal");
+  const state_bird = queryParams.get("state_bird");
+  const state_flower = queryParams.get("state_flower");
+  const state_motto = queryParams.get("state_motto");
+  const state_song = queryParams.get("state_song");
+  const state_history = queryParams.get("state_history");
+  const state_website = queryParams.get("state_website");
+  const state_industries = queryParams.get("state_industries");
+  const state_festivals = queryParams.get("state_festivals");
+
+  const state_school = queryParams.get("state_school");
+  const state_primary = queryParams.get("state_primary");
+  const state_upper = queryParams.get("state_upper");
+  const state_secondary = queryParams.get("state_secondary");
+  const state_higher = queryParams.get("state_higher");
+
+  //...Receiving Data Ends
+
+  document.title = state_name;
+  document.getElementById("statename").textContent = state_name;
+  document.getElementById("statemaplink").href = state_maplink;
+
+  document.getElementById("statecapital").textContent = state_capital;
+  document.getElementById("statecm").textContent = state_cm;
+  document.getElementById("stategovernor").textContent = state_governor;
+  document.getElementById("statedistricts").textContent = state_districts;
+  document.getElementById("statepopulation").textContent = state_populations;
+  document.getElementById("statearea").textContent = state_area;
+  document.getElementById("stateliteracy").textContent = state_literacy;
+  document.getElementById("stategdp").textContent = state_gdp;
+  document.getElementById("statesymbol").textContent = state_symbol;
+  document.getElementById("stateanimal").textContent = state_animal;
+  document.getElementById("statebird").textContent = state_bird;
+  document.getElementById("stateflower").textContent = state_flower;
+  document.getElementById("motto").textContent = state_motto;
+  document.getElementById("statesong").textContent = state_song;
+  document.getElementById("statehistory").textContent = state_history;
+  document.getElementById("websitelink").href = state_website;
+
+  document.getElementById("stateschool").textContent = state_school;
+  document.getElementById("stateprimary").textContent = state_primary;
+  document.getElementById("stateupper").textContent = state_upper;
+  document.getElementById("statesecondary").textContent = state_secondary;
+  document.getElementById("statehigher").textContent = state_higher;
+
+  const carouselImage = document.getElementById("carousel-image");
+  const carouselImage1 = document.getElementById("carousel1-image");
+
+  const indus_arr = JSON.parse(state_industries);
+
+  const p_container = document.getElementById("indus-card");
+  indus_arr.forEach((element) => {
+    const ptag = document.createElement("p");
+    ptag.innerHTML = element;
+    p_container.appendChild(ptag);
+  });
+
+  const festi_arr = JSON.parse(state_festivals);
+
+  const p1_container = document.getElementById("festi-card");
+  festi_arr.forEach((element) => {
+    const ptag1 = document.createElement("p");
+    ptag1.innerHTML = element;
+    p1_container.appendChild(ptag1);
+  });
+
+  const images = JSON.parse(state_imgs);
+  let currentIndex = 0;
+  function changeImage() {
+    carouselImage.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+
+  changeImage();
+
+  // Change image every 3 seconds
+  setInterval(changeImage, 3000);
+
+  // function toggleopenclose() {
+
+  // }
+  console.log(tourist_names);
+
+  const touristnamesarr = JSON.parse(tourist_names);
+  console.log(touristnamesarr);
+  const images1 = JSON.parse(tourist_imgs);
+  let currentIndex1 = 0;
+  function changeImage1() {
+    carouselImage1.src = images1[currentIndex1];
+    const tourist_name_container = document.getElementById("touristnames");
+    tourist_name_container.textContent = touristnamesarr[currentIndex1];
+    [currentIndex1];
+    currentIndex1 = (currentIndex1 + 1) % images1.length;
+  }
+
+  changeImage1();
+
+  // Change image every 3 seconds
+  setInterval(changeImage1, 3000);
+});
---
+++ Know-My-Nation-main/scripts/script-table.js
@@ -0,0 +1,111 @@
+function filterDatatable(stateId) {
+  fetch("/states-2.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const filteredData = data.find((item) => item.stateId === stateId);
+
+      const queryParams = new URLSearchParams();
+
+      // Sending Data...
+
+      queryParams.set("state_name", filteredData.basic_information.name);
+      queryParams.set("state_imgs", JSON.stringify(filteredData.images));
+      queryParams.set("state_maplink", filteredData.statemapurl);
+
+      queryParams.set("state_capital", filteredData.basic_information.capital);
+      queryParams.set(
+        "state_cm",
+        filteredData.basic_information.chief_minister
+      );
+      queryParams.set(
+        "state_governor",
+        filteredData.basic_information.governor
+      );
+      queryParams.set("state_districts", filteredData.demographics.districts);
+      queryParams.set(
+        "state_populations",
+        filteredData.demographics.population
+      );
+      queryParams.set("state_area", filteredData.demographics.area);
+
+      queryParams.set(
+        "state_literacy",
+        filteredData.demographics.literacy_rate
+      );
+      queryParams.set("state_gdp", filteredData.economy.gdp);
+      queryParams.set(
+        "state_symbol",
+        filteredData.culture_and_symbols.state_symbols.emblem
+      );
+      queryParams.set(
+        "state_animal",
+        filteredData.culture_and_symbols.state_animal
+      );
+      queryParams.set(
+        "state_bird",
+        filteredData.culture_and_symbols.state_bird
+      );
+      queryParams.set(
+        "state_flower",
+        filteredData.culture_and_symbols.state_flower
+      );
+
+      queryParams.set("state_motto", filteredData.culture_and_symbols.motto);
+      queryParams.set(
+        "state_song",
+        filteredData.culture_and_symbols.state_song
+      );
+
+      queryParams.set(
+        "state_climate",
+        filteredData.culture_and_symbols.climate
+      );
+
+      queryParams.set(
+        "state_festivals",
+        JSON.stringify(filteredData.culture_and_symbols.major_festivals)
+      );
+
+      queryParams.set(
+        "state_history",
+        filteredData.culture_and_symbols.historical_significance
+      );
+
+      queryParams.set("state_website", filteredData.websiteurl);
+
+      queryParams.set(
+        "state_industries",
+        JSON.stringify(filteredData.economy.major_industries)
+      );
+
+      queryParams.set(
+        "state_school",
+        filteredData.culture_and_symbols.state_school
+      );
+
+      queryParams.set(
+        "state_primary",
+        filteredData.culture_and_symbols.state_primary
+      );
+
+      queryParams.set(
+        "state_upper",
+        filteredData.culture_and_symbols.state_upper
+      );
+
+      queryParams.set(
+        "state_secondary",
+        filteredData.culture_and_symbols.state_secondary
+      );
+
+      queryParams.set(
+        "state_higher",
+        filteredData.culture_and_symbols.state_higher
+      );
+
+      // Sending Data Ends...
+      const url = "/Pages/page.html?" + queryParams.toString();
+      window.location.href = url;
+    })
+    .catch((error) => console.error("Error", error));
+}
